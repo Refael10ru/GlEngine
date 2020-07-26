db479695fcd11ba50982479710f48d120b787032
refael10ru <refael10.ru@gmail.com>
2020-07-26 11:55:40 +0300

    anoher super small fix

--- src/ColoredMesh.cpp
+++ src/ColoredMesh.cpp
@@ -11,14 +11,14 @@ namespace okek
         this->points = reinterpret_cast<ColoredCPoints*>(malloc(sizeVertices*sizeof(ColoredCPoints) + sizeof(unsigned long int) + 1));
         this->points->heap = true;
         this->points->PointsBytes = sizeVertices;
-        memcpy(this->points + sizeof(unsigned long int) + 1,
+        memcpy(this->points->Cpoints,
          vertices, sizeVertices);
 
 
         this->indices = reinterpret_cast<Cmesh_indices*>(malloc(sizeVertices + sizeof(unsigned long int) + 1));
         this->indices->heap = true;
         this->indices->OffsetsBytes = sizeIndices;
-        memcpy(this->indices + sizeof(unsigned long int) + 1,
+        memcpy(this->indices->offsets,
          indices, sizeIndices);
     }
     ColoredMesh::ColoredMesh(ColoredMesh* Target) 
